59fcbbed6fa51dbaaa176b381999b82aef4e207b
Lars Johansson <Johansson@carismar.com>
2020-03-17 08:47:01 +0100

    Translation support

--- components/atoms/CaLogo/CaLogo.vue
+++ components/atoms/CaLogo/CaLogo.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="ca-logo">Logo</div>
+  <div class="ca-logo">
+    <h1>{{ $t("greeting") }}</h1>
+  </div>
 </template>
 <script>
 export default {
--- index.js
+++ index.js
@@ -2,6 +2,4 @@
 // Auto-generated file by create-index-files.js. Do not edit manually
 import CaLogo from "./components/atoms/CaLogo/CaLogo.vue";
 
-export {
-  CaLogo
-};
+export { CaLogo };
