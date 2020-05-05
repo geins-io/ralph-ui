8440647b0fb01fe4dc6ce2cfb6f93aa1e7d3952e
Lars Johansson <johansson@carismar.com>
2020-05-05 16:04:59 +0200

    Product meta schema

--- components/atoms/CaProductMeta/CaProductMeta.vue
+++ components/atoms/CaProductMeta/CaProductMeta.vue
@@ -1,5 +1,53 @@
 <template>
-  <div class="ca-product-meta"></div>
+  <script type="application/ld+json">
+    {
+      "@context": "https://schema.org/",
+      "@type": "Product",
+      "name": "Name",
+      "image": [
+        "https://example.com/photos/1x1/photo.jpg",
+        "https://example.com/photos/4x3/photo.jpg",
+        "https://example.com/photos/16x9/photo.jpg"
+      ],
+      "description": "Sleeker than ACME's Classic Anvil, the Executive Anvil is perfect for the business traveler looking for something to drop from a height.",
+      "sku": "0446310786",
+      "mpn": "925872",
+      "brand": {
+        "@type": "Brand",
+        "name": "ACME"
+      },
+      "review": {
+        "@type": "Review",
+        "reviewRating": {
+          "@type": "Rating",
+          "ratingValue": "4",
+          "bestRating": "5"
+        },
+        "author": {
+          "@type": "Person",
+          "name": "Fred Benson"
+        }
+      },
+      "aggregateRating": {
+        "@type": "AggregateRating",
+        "ratingValue": "4.4",
+        "reviewCount": "89"
+      },
+      "offers": {
+        "@type": "Offer",
+        "url": "https://example.com/anvil",
+        "priceCurrency": "USD",
+        "price": "119.99",
+        "priceValidUntil": "2020-11-20",
+        "itemCondition": "https://schema.org/UsedCondition",
+        "availability": "https://schema.org/InStock",
+        "seller": {
+          "@type": "Organization",
+          "name": "Executive Objects"
+        }
+      }
+    }
+  </script>
 </template>
 <script>
 // @group Atoms
@@ -8,7 +56,13 @@ export default {
   name: 'CaProductMeta',
   components: {},
   mixins: [],
-  props: {},
+  props: {
+    // Product object
+    product: {
+      type: Object,
+      required: true
+    }
+  },
   data: () => ({}),
   computed: {},
   watch: {},
