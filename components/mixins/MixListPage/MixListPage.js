336201fcea637972245614ef3af33f9ad6ec03b1
Olivia Axelsson <olivia.axelsson@gmail.com>
2020-10-06 18:15:52 +0200

    changed name of mixlistpage

--- components/mixins/MixListPage/MixListPage.js
+++ components/mixins/MixListPage/MixListPage.js
@@ -18,7 +18,64 @@
 export default {
   components: {},
   mixins: [],
-  props: {},
+  apollo: {
+    products: {
+      query() {
+        return this.productsQuery;
+      },
+      variables() {
+        return this.productsQueryVars;
+      },
+      deep: true,
+      result(result) {
+        this.productList = result.data.products.products;
+        this.totalCount = result.data.products.count;
+        this.filters = result.data.products.filters;
+        this.setInitPriceSelection(
+          result.data.products.filters.price.lowest,
+          result.data.products.filters.price.highest
+        );
+        if (this.$store.getters['list/relocateProduct']) {
+          this.relocateProduct();
+        }
+        if (this.currentMaxCount > this.totalCount) {
+          this.currentMaxCount = this.totalCount;
+        }
+      },
+      skip() {
+        return this.skipProductsQuery;
+      }
+    },
+    listPageInfo: {
+      query() {
+        return this.infoQuery;
+      },
+      variables() {
+        return this.infoQueryVars;
+      },
+      result(result) {
+        this.listInfo = result.data.listPageInfo;
+      }
+    }
+  },
+  props: {
+    type: {
+      type: String,
+      default: 'category' // 'brand', 'search', 'favorites'
+    },
+    infoQuery: {
+      type: Object,
+      required: true
+    },
+    productsQuery: {
+      type: Object,
+      required: true
+    },
+    currentAlias: {
+      type: String,
+      required: true
+    }
+  },
   data: () => ({
     productList: [],
     totalCount: 0,
@@ -74,10 +131,82 @@ export default {
         queryObj.page = this.currentPage.toString();
       }
       return queryObj;
+    },
+    modifier() {
+      return 'ca-list-page--' + this.type;
+    },
+    isCategory() {
+      return this.type === 'category';
+    },
+    isBrand() {
+      return this.type === 'brand';
+    },
+    infoQueryVars() {
+      const varsObj = {
+        apiKey: this.$store.getters.currentApiKey
+      };
+      if (this.isCategory) {
+        this.$set(varsObj, 'categoryAlias', this.currentAlias);
+      }
+      if (this.isBrand) {
+        this.$set(varsObj, 'brandAlias', this.currentAlias);
+      }
+      return varsObj;
+    },
+    productsQueryVars() {
+      const varsObj = {
+        skip: this.skip,
+        take: this.pageSize,
+        apiKey: this.$store.getters.currentApiKey,
+        sort: this.sort,
+        filter: this.selection
+      };
+      if (this.isCategory) {
+        this.$set(varsObj, 'categoryAlias', this.currentAlias);
+      }
+      if (this.isBrand) {
+        this.$set(varsObj, 'brandAlias', this.currentAlias);
+      }
+      return varsObj;
+    },
+    loadMoreQueryVars() {
+      const varsObj = {
+        skip: this.currentMaxCount,
+        take: this.pageSize,
+        apiKey: this.$store.getters.currentApiKey,
+        sort: this.sort,
+        filter: this.selection
+      };
+      if (this.isCategory) {
+        this.$set(varsObj, 'categoryAlias', this.currentAlias);
+      }
+      if (this.isBrand) {
+        this.$set(varsObj, 'brandAlias', this.currentAlias);
+      }
+      return varsObj;
+    },
+    loadPrevQueryVars() {
+      const varsObj = {
+        skip: this.currentMinCount - 1 - this.pageSize,
+        take: this.pageSize,
+        apiKey: this.$store.getters.currentApiKey,
+        sort: this.sort,
+        filter: this.selection
+      };
+      if (this.isCategory) {
+        this.$set(varsObj, 'categoryAlias', this.currentAlias);
+      }
+      if (this.isBrand) {
+        this.$set(varsObj, 'brandAlias', this.currentAlias);
+      }
+
+      return varsObj;
     }
   },
   watch: {},
-  created() {},
+  created() {
+    this.initProductList();
+  },
   mounted() {},
   methods: {
     // @vuese
