5317a067a86edc7d1ed3e52bc806cbcf6945caac
Lars Johansson <johansson@carismar.com>
2021-01-14 10:46:28 +0100

    pascalCased widget configuration json because API changes

--- components/molecules/CaWidgetButtons/CaWidgetButtons.vue
+++ components/molecules/CaWidgetButtons/CaWidgetButtons.vue
@@ -30,7 +30,7 @@ export default {
   data: () => ({}),
   computed: {
     buttons() {
-      return JSON.parse(this.configuration.Buttons);
+      return JSON.parse(this.configuration.buttons);
     }
   },
   watch: {},
