eee72edc638bb1f7d064e3b880e9875facfb0317
Lars Johansson <johansson@carismar.com>
2021-01-22 11:22:01 +0100

    Improved flag component documentation

--- components/atoms/CaFlag/CaFlag.vue
+++ components/atoms/CaFlag/CaFlag.vue
@@ -7,7 +7,7 @@
 </template>
 <script>
 // @group Atoms
-// This component can display a flag based on the [ISO 3166-1-alpha-2 code](https://www.iso.org/obp/ui/#search/code/) for the country.<br><br>
+// This component can display a flag based on the [ISO 3166-1-alpha-2 code](https://www.iso.org/obp/ui/#search/code/) for the country.<br><br> Takes flags from the static folder of the main project. Build for, and recommend using with, flags from [flag-icon-css] (https://github.com/lipis/flag-icon-css)
 // **SASS-path:** _./styles/components/atoms/ca-flag.scss_
 export default {
   name: 'CaFlag',
