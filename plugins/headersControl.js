e0dd3bada7fcb4149e7b8ee0f849c2cf93d895e1
Maveldous <arttime32@gmail.com>
2022-05-09 16:28:55 +0300

    add logs

--- plugins/headersControl.js
+++ plugins/headersControl.js
@@ -1,4 +1,5 @@
 export default ({ req, store }) => {
   const headers = req && req.headers ? Object.assign({}, req.headers) : {};
+  console.log(req.headers, headers);
   store.commit('setChannelId', headers['X-channelId'] || 'MockId');
 };
