aa6c409b59b3e38a2be7603e499f825b2549127e
Kevin Fitzpatrick <fitzpatrick@carismar.com>
2022-09-28 13:41:12 +0200

    fix(filter-panel): Adjusted unnecessary arr arguments

--- components/organisms/CaFilterPanel/CaFilterPanel.vue
+++ components/organisms/CaFilterPanel/CaFilterPanel.vue
@@ -9,7 +9,7 @@
     <LazyCaAccordionItem
       v-if="
         filters.categories &&
-          filters.categories.length > 1 &&
+          filters.categories.length &&
           $config.showCategoryFilter &&
           $config.showCategoryTreeViewFilter
       "
@@ -36,7 +36,7 @@
     <LazyCaAccordionItem
       v-if="
         filters.categories &&
-          filters.categories.length > 1 &&
+          filters.categories.length &&
           $config.showCategoryFilter &&
           !$config.showCategoryTreeViewFilter
       "
@@ -61,9 +61,7 @@
       />
     </LazyCaAccordionItem>
     <LazyCaAccordionItem
-      v-if="
-        filters.brands && filters.brands.length > 1 && $config.showBrandsFilter
-      "
+      v-if="filters.brands && filters.brands.length && $config.showBrandsFilter"
       class="ca-filter-panel__toggle"
       :open-on-init="contentpanel.frame === 'brands'"
     >
@@ -85,7 +83,7 @@
       />
     </LazyCaAccordionItem>
     <LazyCaAccordionItem
-      v-if="filters.skus && filters.skus.length > 1 && $config.showSkuFilter"
+      v-if="filters.skus && filters.skus.length && $config.showSkuFilter"
       class="ca-filter-panel__toggle"
       :open-on-init="contentpanel.frame === 'skus'"
     >
@@ -107,9 +105,7 @@
       />
     </LazyCaAccordionItem>
     <LazyCaAccordionItem
-      v-if="
-        filters.price && filters.price.length > 1 && $config.showPriceFilter
-      "
+      v-if="filters.price && filters.price.length && $config.showPriceFilter"
       class="ca-filter-panel__toggle"
       :open-on-init="contentpanel.frame === 'price'"
     >
@@ -158,7 +154,7 @@
     <LazyCaAccordionItem
       v-if="
         filters.discount &&
-          filters.discount.length > 1 &&
+          filters.discount.length &&
           $config.showDiscountFilter
       "
       class="ca-filter-panel__toggle"
