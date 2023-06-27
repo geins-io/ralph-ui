795b037dd9ccf9d13b56acb78a2c588839d4d4f0
arvidsson-geins <arvidsson@geins.io>
2023-06-27 15:04:20 +0200

    add: json-widget

---
+++ components/molecules/CaWidgetJson/CaWidgetJson.spec.js
@@ -0,0 +1,8 @@
+import { shallowMount } from '@vue/test-utils';
+import CaWidgetJson from './CaWidgetJson.vue';
+
+describe('CaWidgetJson.vue', () => {
+  it('renders a component', () => {
+    const component = shallowMount(CaWidgetJson);
+  });
+});
---
+++ components/molecules/CaWidgetJson/CaWidgetJson.vue
@@ -0,0 +1,28 @@
+<template>
+   <code
+      v-if="configuration.JSON"
+      v-html="configuration.JSON"
+    >
+    </code>
+</template>
+<script>
+/* 
+  Custom widget component that renders a widget based on the configuration JSON object
+*/
+export default {
+  name: 'CaWidgetJson',
+  mixins: [],
+  props: {
+    // Widget configuration object
+    configuration: {
+      type: Object,
+      required: true
+    }
+  },
+  data: () => ({}),
+  computed: {},
+  watch: {},
+  mounted() {},
+  methods: {}
+};
+</script>
