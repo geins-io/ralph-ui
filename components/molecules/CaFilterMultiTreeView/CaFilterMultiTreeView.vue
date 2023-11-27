011ff5b89b1d85725f60d61a85639c75dfa6716f
Isabelle Johansson <isabelle.johansson@geins.io>
2023-11-27 07:33:19 +0100

    fix(filter-multi): unselect children when parent is unselected

--- components/molecules/CaFilterMultiTreeView/CaFilterMultiTreeView.vue
+++ components/molecules/CaFilterMultiTreeView/CaFilterMultiTreeView.vue
@@ -12,7 +12,6 @@
     />
   </ul>
 </template>
-
 <script>
 // @group Molecules
 // @vuese
@@ -56,10 +55,13 @@ export default {
           let isSelected = this.currentSelection.some(
             el => el.id === item.facetId
           );
+
           if (item.count === 0) {
             isSelected = false;
           }
+
           this.$set(item, 'selected', isSelected);
+
           return item;
         });
       } else {
@@ -72,6 +74,7 @@ export default {
     valuesWithChildren() {
       const data = this.sortByOrder(this.filterValues);
       const parentCategories = data.filter(item => !item.parentId);
+
       data.map(item => {
         item.children = data.filter(child => child.parentId === item.facetId);
       });
@@ -127,10 +130,12 @@ export default {
         if (children && children.length) {
           this.selectChildrenCategories(children, facetId, label, selected);
         }
-      } else if (parentId) {
-        this.removeParentCategories(facetId, parentId);
       } else {
         this.removeSelection(facetId);
+
+        if (parentId) {
+          this.removeParentCategories(facetId, parentId);
+        }
 
         if (children && children.length) {
           this.removeChildrenCategories(children, facetId, label, selected);
@@ -199,7 +204,6 @@ export default {
   }
 };
 </script>
-
 <style lang="scss">
 @import 'molecules/ca-filter-multi-tree-view';
 </style>
